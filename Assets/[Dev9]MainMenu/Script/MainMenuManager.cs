846812a2e406784b19c91461820f3dcf8fe9bb28
Trappist-1d-CTR <multicanal11@gmail.com>
2026-04-11 12:56:21 +0200

    Main Menu - Panels & Credits
    - Added a system to quickly change from one "panel" to the other (as in to change from the menu to settings and back, or to credits, etc.)
    - Added the credits panel

--- Assets/[Dev9]MainMenu/Script/MainMenuManager.cs
+++ Assets/[Dev9]MainMenu/Script/MainMenuManager.cs
@@ -7,21 +7,41 @@ public class MainMenuManager : MonoBehaviour
 {
     #region Variables
 
+    #region Menu Navigation Values
+    public int MenuPosition;
+    #endregion
+
     #region Panels
-    public GameObject MenuPanel;
-    public GameObject SettingsPanel;
-    public GameObject CreditsPanel;
+    public List<GameObject> MenuPanels;
     #endregion
 
     #endregion
 
-    void Awake()
+    void Start()
     {
-        
+        SelectPanel(0);
     }
 
-    void Update()
+    public void ResetPanels()
     {
-        
+        for (int i = 0; i < MenuPanels.Count; i++)
+        {
+            MenuPanels[i].SetActive(false);
+        }
+    }
+
+    public void Panel()
+    {
+        ResetPanels();
+        if (!MenuPanels[MenuPosition].activeSelf)
+        {
+            MenuPanels[MenuPosition].SetActive(true);
+        }
+    }
+
+    public void SelectPanel(int ID)
+    {
+        MenuPosition = ID;
+        Panel();
     }
 }
