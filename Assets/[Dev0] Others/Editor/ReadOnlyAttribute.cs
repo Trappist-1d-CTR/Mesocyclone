51c4a08012e092fe119ce828a42ee155e50d015f
half bread <oznerol.discord.gg@gmail.com>
2026-07-12 00:17:43 +0200

    Realized ReadOnlyAttribute should inherit PropertyAttribute and not Attribute

--- Assets/[Dev0] Others/Editor/ReadOnlyAttribute.cs
+++ Assets/[Dev0] Others/Editor/ReadOnlyAttribute.cs
@@ -1,6 +1,7 @@
 using System;
+using UnityEngine;
 
 /// <summary>
 /// Marks a field as read-only in the Unity Inspector
 /// </summary>
-public sealed class ReadOnlyAttribute : Attribute { }
+public sealed class ReadOnlyAttribute : PropertyAttribute { }
